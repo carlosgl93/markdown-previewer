115a1db4ab6283120de006322a4bcdfd2906e808
Carlos Gumucio Labbé <cgumuciolabbe@gmail.com>
2022-10-04 22:29:45 -0300

    First commit

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head></Head>
+      <body>
+        <Main />
+        <NextScript />
+        <script src='https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js' />
+      </body>
+    </Html>
+  );
+}
